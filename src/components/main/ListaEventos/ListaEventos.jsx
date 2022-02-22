bae0fe824885a9b4ed2fcf41e7b22f6c2e44fbf9
guido <guido_fortunato@hotmail.com.ar>
2022-02-22 01:44:58 -0300

    cambio en los eventos

--- src/components/main/ListaEventos/ListaEventos.jsx
+++ src/components/main/ListaEventos/ListaEventos.jsx
@@ -9,7 +9,7 @@ const ListaEventos = ({id, title, image, date, time, buy}) => {
     
     return (
         <>                                       
-                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 text-center mt-2 mb-2" key={id}>
+                    {/* <div className="col-12 col-sm-6 col-md-4 col-lg-3 text-center mt-2 mb-2" key={id}>
                         <div className="card">                                            
                             <img src={`https://tuentrada.com/${image}`} className="card-img-top" alt={title} />
                             <div className="card-body">                                                
@@ -47,7 +47,7 @@ const ListaEventos = ({id, title, image, date, time, buy}) => {
                                     <div className="container-tuentrada__col2">
 
                                         
-                                        {/* <a
+                                        <a
                                             className="btn btn-primary btn-color mt-4"
                                             rel="noreferrer"
                                             target='_blank'
@@ -55,22 +55,54 @@ const ListaEventos = ({id, title, image, date, time, buy}) => {
                                             
                                             >
                                             Comprar
-                                        </a> */}
-                                        <Link
-                                            className="btn btn-primary btn-color mt-4"
-                                            to={`/${id}`}
-                                            
-                                            >
-                                            Comprar
-                                        </Link>
+                                        </a>
 
                                     </div>
                                 </div>
                                 
                             </div>
                         </div>
-                    </div>
+                    </div> */}
+
+                                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 text-center mt-2 mb-2" key={id}>
+                                        <div className="card">                                            
+                                            <img src={`https://tuentrada.com/${image}`} className="card-img-top" alt={title} />
+                                            <div className="card-body">                                                
+                                                <h6 className={'card-title'}>{title}</h6>
+
+                                                <div className="fecha-hora">
+                                                    <i className="bi bi-calendar2-check-fill color-icono me-2"></i>
+                                                    <span className='fecha-hora__color-texto'>Fecha:</span>
+                                                    <span className='ms-1'>{date}</span>
+                                                </div>
+
+                                                <div className="fecha-hora mt-1">
+                                                    <i className="bi bi-clock-fill color-icono me-2"></i>
+                                                    <span className='fecha-hora__color-texto'>Hora:</span>
+                                                    <span className='ms-1'>{time} hs</span>
+                                                </div>
+                                                {/* <Link
+                                                    to={`/${id}`}
+                                                    className="btn btn-color text-white btn-md mt-3"
+                                                    
+                                                >
+                                                    Comprar
+                                                </Link> */}
+                                                <a
+                                                    className="btn btn-primary btn-color mt-4"
+                                                    rel="noreferrer"
+                                                    target='_blank'
+                                                    href={buy}
+                                            
+                                                >
+                                                    Comprar
+                                                </a>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
                 
+                    
         </>
     )
 }
