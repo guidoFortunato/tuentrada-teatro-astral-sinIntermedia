0436cc6098a2e4fb72b68539056549a2ac4b447c
guido <guido_fortunato@hotmail.com.ar>
2022-02-22 01:46:23 -0300

    cambio en los eventos

--- src/components/main/ListaEventos/ListaEventos.jsx
+++ src/components/main/ListaEventos/ListaEventos.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import './listaEventos.css'
 
 const ListaEventos = ({id, title, image, date, time, buy}) => {
