f16f876de21bfbca69f15f72d17df5fbe7560032
guido <guido_fortunato@hotmail.com.ar>
2022-03-30 12:02:32 -0300

    cambios

--- src/components/error/MessageError.jsx
+++ src/components/error/MessageError.jsx
@@ -11,7 +11,7 @@ const MessageError = ({error}) => {
         <div className="container">
             <div className="container-alerta alerta-danger" role="alert">
                 {
-                    error ? <h4 className='text-center'>{`Error ${error.status}: ${error.statusText}`}</h4> : <h4 className='text-center'>No se pudieron cargar los shows, por favor intente mas tarde nuevamente</h4>
+                    error ? <h4 className='text-center'>No hay shows disponibles en este momento</h4> : <h4 className='text-center'>No hay shows disponibles en este momento</h4>
                 }
             </div>
         </div>
